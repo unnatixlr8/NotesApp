7b7e4a36274343fe457843a6d3b18c40f2e90278
Unnati <unnatixlr8@gmail.com>
2019-02-18 00:20:52 +0530

    added basic view

--- view.py
+++ view.py
@@ -14,7 +14,13 @@ class ViewWindow():
 
 	def noteBox(self,master):
 		self.master = master
-		strVar = StringVar()
+		scrollBar = Scrollbar(master)
+		canvas = Canvas(master,background = "#D2D2D2",yscrollcommand=scrollBar.set)
+		scrollBar.config(command=canvas.yview)
+		scrollBar.pack(side=RIGHT, fill=Y)
+		windowFrame = Frame(canvas)
+		canvas.pack(side="left", fill="both", expand=True) 
+		canvas.create_window(0,0,window=windowFrame, anchor='nw')
 
 		try:
 			conn = mysql.connector.connect(user='root', password='pythondb',host='127.0.0.1',database='python')
@@ -24,8 +30,22 @@ class ViewWindow():
 			results = cursor.fetchall()
 			print("Data fetched")
 			for row in results:
-				#print(row[0]+"	"+row[1]+"	"+row[2])
-				print(row)
+				print(str(row[0])+"	"+str(row[1])+"	"+str(row[2]))
+				idStrVar = StringVar(value=str(row[0]))
+				timeStampStrVar = StringVar(value=str(row[1]))
+				noteStrVar = StringVar(value=str(row[2]))
+				idLabel = Label(windowFrame, textvariable=idStrVar,font=("Verdana",18))
+				idLabel.pack()
+				timeStampLabel = Label(windowFrame, textvariable=timeStampStrVar, font=("Verdana",18))
+				timeStampLabel.pack()
+				noteLabel = Label(windowFrame, wraplength=600, textvariable=noteStrVar, font=("Verdana",18))
+				noteLabel.pack()
+				Label(windowFrame,text="\n").pack()
+
+			master.update()
+			canvas.config(scrollregion=canvas.bbox("all"))
+
+
 
 		except mysql.connector.Error as error:
 			print("Error")
